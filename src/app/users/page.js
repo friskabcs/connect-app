c5ceddfcdfad0b5f30614f5d2cc997f70475d489
gandunganggoro <gandunganggoro@gmail.com>
2025-04-23 10:59:17 +0800

    first commit

---
+++ src/app/users/page.js
@@ -0,0 +1,12 @@
+export default function UsersPage() {
+    return (
+        <div>
+            <div>
+                <h1>Connect</h1>
+            </div>
+            <div>
+            <button type="button">User</button>
+            </div>
+        </div>
+    );
+}
