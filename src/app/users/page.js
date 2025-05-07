293eb63bb67de8f1546333c55897c3abd52b8c0d
friskabcs <friskafriskabpp12@gmail.com>
2025-05-07 12:39:41 +0800

    feat : create user page

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,12 +1,25 @@
-export default function UsersPage() {
+import { IconKey, IconLogout, IconUser } from "@tabler/icons-react";
+export default function Users_Page() {
     return (
-        <div>
-            <div>
-                <h1>Connect</h1>
-            </div>
-            <div>
-            <button type="button">User</button>
-            </div>
+        <div id="container" className="flex h-[100vh]">
+            <section id="navigation" className="bg-gray-400 p-4 w-[200px]">
+                <h1 className="text-3xl font-bold text-center">Connect</h1>
+                <div className="flex flex-col gap-2 mt-4">
+                    <button className="flex gap-2"> 
+                        <IconUser /> Users
+                        </button>
+                    <button className="flex gap-2">
+                    <IconKey /> Hak Akses
+                    </button>
+                    <button className="flex gap-2"> 
+                    <IconLogout />Logout
+                    </button>
+                </div>
+            </section>
+            <section id="content">
+                <input type="text" placeholder="Cari Users" />
+                <div id="list-users"></div>
+            </section>
         </div>
     );
 }
