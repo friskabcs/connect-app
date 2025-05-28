806bfec059ac3269afdc34e30c94eb118b3f1bef
friskabcs <friskafriskabpp12@gmail.com>
2025-05-28 13:08:03 +0800

    feat : create user page

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,25 +1,48 @@
-import { IconKey, IconLogout, IconUser } from "@tabler/icons-react";
+import UserCard from "@/components/ui/user-card";
+import { dataUser } from "@/mock/data-user";
+import { IconKey, IconLogout, IconUser, IconPlus } from "@tabler/icons-react";
+
 export default function Users_Page() {
-    return (
-        <div id="container" className="flex h-[100vh]">
-            <section id="navigation" className="bg-gray-400 p-4 w-[200px]">
-                <h1 className="text-3xl font-bold text-center">Connect</h1>
-                <div className="flex flex-col gap-2 mt-4">
-                    <button className="flex gap-2"> 
-                        <IconUser /> Users
-                        </button>
-                    <button className="flex gap-2">
-                    <IconKey /> Hak Akses
-                    </button>
-                    <button className="flex gap-2"> 
-                    <IconLogout />Logout
-                    </button>
-                </div>
-            </section>
-            <section id="content">
-                <input type="text" placeholder="Cari Users" />
-                <div id="list-users"></div>
-            </section>
+  const data = dataUser;
+
+  return (
+    <div id="container" className="flex h-screen bg-white font-sans text-black">
+      <aside className="w-[220px] bg-white border-r border-gray-600 py-4 p-8 gap-6 flex flex-col">
+        <h1 className="text-3xl font-bold text-center">Connect</h1>
+        <div className="flex flex-col gap-2 mt-4">
+          <button className="flex gap-2 items-center px-2 py-1 hover:bg-gray-100 rounded bg-black text-white">
+            <IconUser size={20} /> Users
+          </button>
+          <button className="flex gap-2 items-center px-2 py-1 hover:bg-gray-100 rounded text-black">
+            <IconKey size={20} /> Hak Akses
+          </button>
+          <button className="flex gap-2 items-center px-2 py-1 hover:bg-gray-100 rounded text-black">
+            <IconLogout size={20} /> Logout
+          </button>
         </div>
-    );
+      </aside>
+
+      <section id="content" className="flex-1 p-6 relative text-black">
+        <input
+          type="text"
+          placeholder="Cari Users"
+          className="w-full border px-4 py-2 rounded mb-6 placeholder-gray-600 text-black"
+        />
+        <div id="list-users" className="flex flex-col gap-4">
+          {data.map((employee, index) => (
+            <UserCard
+              key={index}
+              fullname={employee.fullname}
+              email={employee.email}
+              role={employee.role}
+              status={employee.status}
+            />
+          ))}
+        </div>
+        <button className="absolute bottom-6 right-6 bg-gray-200 hover:bg-gray-300 p-3 rounded shadow text-black">
+          <IconPlus size={20} />
+        </button>
+      </section>
+    </div>
+  );
 }
