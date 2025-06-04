894d6e942d90bf014c524a7aef634e7d9e3a201b
friskabcs <friskafriskabpp12@gmail.com>
2025-06-04 12:45:44 +0800

    feat : create user page

---
+++ src/app/admin/roles/page.js
@@ -0,0 +1,12 @@
+export default function RolesPage() {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Cari Hak Akses"
+        className="w-full border px-4 py-2 rounded mb-6 placeholder-gray-600 text-black"
+      />
+      <p>Ini adalah tabel roles</p>
+    </div>
+  );
+}
